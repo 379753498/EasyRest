5800644698bf58ab613324df649e1a37d8ba06c6
Testdev <379753498@qq.com>
2018-12-21 17:44:24 +0800

    测试测试报告生成情况

--- src/test/java/com/testpro/easyrest/EasyrestApplicationTests.java
+++ src/test/java/com/testpro/easyrest/EasyrestApplicationTests.java
@@ -3,7 +3,10 @@ package com.testpro.easyrest;
 import com.testpro.easyrest.Core.interfaceExecution;
 import com.testpro.easyrest.Data.InterfaceDataProvider;
 import com.testpro.easyrest.bean.ExcelData;
+import io.qameta.allure.Description;
+import io.qameta.allure.Features;
 import io.qameta.allure.Step;
+import io.qameta.allure.Stories;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
@@ -28,7 +31,7 @@ public class EasyrestApplicationTests extends AbstractTestNGSpringContextTests {
 
 
     @Test(testName = "esayrestTests", dataProvider = "getdata")
-    @Step
+    @Step("测试用例名称：{excelData.caseDescription}")
     public void contextLoads(ExcelData excelData) {
         interfaceExecution.execution(excelData);
 
