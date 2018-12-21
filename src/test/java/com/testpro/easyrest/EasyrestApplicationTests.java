6e354d45edb06c9e0b004f5a6130d11e52666d30
379753498 <xujian123>
2018-12-21 17:55:43 +0800

    修改测试类

--- src/test/java/com/testpro/easyrest/EasyrestApplicationTests.java
+++ src/test/java/com/testpro/easyrest/EasyrestApplicationTests.java
@@ -3,10 +3,7 @@ package com.testpro.easyrest;
 import com.testpro.easyrest.Core.interfaceExecution;
 import com.testpro.easyrest.Data.InterfaceDataProvider;
 import com.testpro.easyrest.bean.ExcelData;
-import io.qameta.allure.Description;
-import io.qameta.allure.Features;
 import io.qameta.allure.Step;
-import io.qameta.allure.Stories;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
@@ -18,9 +15,10 @@ import java.util.Iterator;
 @SpringBootTest
 public class EasyrestApplicationTests extends AbstractTestNGSpringContextTests {
 
-
+    //数据发动机
     @Autowired
     InterfaceDataProvider interfaceDataProvider;
+    // 执行驱动器
     @Autowired
     interfaceExecution interfaceExecution;
 
