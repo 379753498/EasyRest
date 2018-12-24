50e3717338fb1c9b290069d535ac3da5f117c0d9
379753498 <xujian123>
2018-12-24 11:47:55 +0800

    修复头信息和参数信息未填写的空指针异常

--- src/main/java/com/testpro/easyrest/Core/imp/RestAssuredImp.java
+++ src/main/java/com/testpro/easyrest/Core/imp/RestAssuredImp.java
@@ -13,7 +13,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Component
 @Slf4j
@@ -23,7 +25,7 @@ public class RestAssuredImp implements interfaceExecution {
     private InitialConfiguration initialConfiguration;
 
     @Resource
-    private  ResponseExecut responseExecut;
+    private ResponseExecut responseExecut;
     @Resource
     private Verification verification;
 
@@ -40,25 +42,37 @@ public class RestAssuredImp implements interfaceExecution {
 
         Response response = responseExecut.execut(executionData);
 //      如果预期结果是json则展示在报告中
-       if (executionData.getRetruntype().equals("json")){
-           ReportDetil.respondBody(response.asString());
-       }
+        if (executionData.getRetruntype().equals("json")) {
+            ReportDetil.respondBody(response.asString());
+        }
 
         verification.executVerification(response, executionData);
     }
 
-       private   String rquestbodyData(ExecutionData executionData){
-           LinkedHashMap<String,Object> stringMap= new LinkedHashMap<>();
-             String returntype = executionData.getRetruntype();
-             String method = executionData.getMethod();
-             String headers = executionData.getHeaders();
-             String parameters = executionData.getParameters();
-             String url = executionData.getUrl();
-             stringMap.put("URL",url);
-             stringMap.put("Method",method);
-             stringMap.put("参数列表", JsonUtil.FastStringtoMap(parameters));
-             stringMap.put("头信息",JsonUtil.FastStringtoMap(headers));
-             stringMap.put("返回类型",returntype);
-             return JsonUtil.FastObjectToJsonString(stringMap);
-         }
+    private String rquestbodyData(ExecutionData executionData) {
+        LinkedHashMap <String, Object> stringMap = new LinkedHashMap <>();
+        String returntype = executionData.getRetruntype();
+        String method = executionData.getMethod();
+        String headers = executionData.getHeaders();
+        String parameters = executionData.getParameters();
+        String url = executionData.getUrl();
+        stringMap.put("URL", url);
+        stringMap.put("Method", method);
+        if (parameters != null) {
+            stringMap.put("参数列表", JsonUtil.FastStringtoMap(parameters));
+        } else {
+            Map map = new HashMap();
+            map.put("参数信息", "无");
+            stringMap.put("参数列表", map);
+        }
+        if (headers != null) {
+            stringMap.put("头信息", JsonUtil.FastStringtoMap(headers));
+        } else {
+            Map map = new HashMap();
+            map.put("头信息", "无");
+            stringMap.put("头信息列表", map);
+        }
+        stringMap.put("返回类型", returntype);
+        return JsonUtil.FastObjectToJsonString(stringMap);
+    }
 }
