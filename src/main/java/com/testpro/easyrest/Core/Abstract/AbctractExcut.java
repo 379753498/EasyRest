03ab22587f910a2b5a51948d91b2b74c953b4f5a
Testdev <379753498@qq.com>
2018-12-26 13:34:05 +0800

    修复抽象返回值错误

--- src/main/java/com/testpro/easyrest/Core/Abstract/AbctractExcut.java
+++ src/main/java/com/testpro/easyrest/Core/Abstract/AbctractExcut.java
@@ -4,7 +4,6 @@ import com.testpro.easyrest.Core.Interface.InterfaceExecution;
 import com.testpro.easyrest.Core.Interface.ResponseExecut;
 import com.testpro.easyrest.Core.Interface.Verification;
 import com.testpro.easyrest.bean.ExecutionData;
-import io.restassured.response.Response;
 
 /**
  * 聚合接口抽象类
@@ -12,16 +11,16 @@ import io.restassured.response.Response;
  * @param <T> 请求返回参数类
  * @param <E> 执行参数类型
  */
-abstract class AbctractExcut<T, E extends ExecutionData> implements InterfaceExecution <E>, ResponseExecut <T, E>, Verification <T, E> {
+public abstract class AbctractExcut<T, E extends ExecutionData> implements InterfaceExecution <E>, ResponseExecut <T, E>, Verification <T, E> {
 
 
-    public abstract Response UrlHeadParam(E executionData);
+    public abstract T UrlHeadParam(E executionData);
 
-    public abstract Response UrlParam(E executionData);
+    public abstract T UrlParam(E executionData);
 
-    public abstract Response UrlHead(E executionData);
+    public abstract T UrlHead(E executionData);
 
-    public abstract Response Url(E executionData);
+    public abstract T Url(E executionData);
 
     public abstract void ResponseVauleCheck(T response , E executionData);
 
