72a776ebafa45a3d71e27d5c79e31552726c275e
HelloWord <379753498@qq.com>
2018-12-29 21:32:35 +0800

    1.1.0版本去除不必要依赖发布到中央仓库

--- src/test/java/com/testpro/easyrest/EasyrestApplicationTests.java
+++ src/test/java/com/testpro/easyrest/EasyrestApplicationTests.java
@@ -1,37 +1,29 @@
-//package com.testpro.easyrest;
-//
-//import com.testpro.easyrest.Core.Interface.InterfaceDataProvider;
-//import com.testpro.easyrest.Core.Interface.InterfaceExecution;
-//import com.testpro.easyrest.bean.ExecutionData;
-//import io.qameta.allure.Step;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-//import org.testng.annotations.DataProvider;
-//import org.testng.annotations.Test;
-//
-//import java.util.Iterator;
-//
-//@SpringBootTest
-//public class EasyrestApplicationTests extends AbstractTestNGSpringContextTests {
-//
-//    //数据发动机
-//    @Autowired
-//    InterfaceDataProvider interfaceDataProvider;
-//    // 执行驱动器
-//    @Autowired
-//    InterfaceExecution interfaceExecution;
-//
-//    @DataProvider(name = "getdata")
-//    public Iterator <Object[]> getdata() {
-//        return interfaceDataProvider.ImplementDataProvider();
-//    }
-//
-//    @Test(testName = "esayrestTests", dataProvider = "getdata")
-//    @Step("测试用例名称：{executionData.caseDescription}")
-//    public void EasyRestApplicationContext(ExecutionData executionData) {
-//        interfaceExecution.execution(executionData);
-//    }
-//
-//}
-//
+package com.testpro.easyrest;
+
+
+import com.testpro.easyrest.Core.imp.RestAssuredImp;
+import com.testpro.easyrest.Data.ExcelInterfaceDataProd;
+import com.testpro.easyrest.bean.ExecutionData;
+import io.qameta.allure.Step;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Iterator;
+
+public class EasyrestApplicationTests  {
+
+
+
+    @DataProvider(name = "getdata")
+    public Iterator <Object[]> getdata() {
+         return new ExcelInterfaceDataProd().ImplementDataProvider();
+    }
+
+    @Test(testName = "esayrestTests", dataProvider = "getdata")
+    @Step("测试用例名称：{executionData.caseDescription}")
+    public void EasyRestApplicationContext(ExecutionData executionData) {
+        new RestAssuredImp().execution(executionData);
+    }
+
+}
+
